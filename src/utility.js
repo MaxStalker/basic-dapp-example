fb4f01ce6f10c9d65a21125bf27fa2424d858be3
Max Daunarovich <max.daunarovich@gmail.com>
2020-07-17 14:54:27 +0300

    Create utility methods

---
+++ src/utility.js
@@ -0,0 +1,36 @@
+const urlParams = {
+  url: "",
+  params: {},
+};
+export const getUrlParam = (name) => {
+  const currentUrl = window.location.href || "";
+  if (urlParams.url !== currentUrl) {
+    urlParams.url = currentUrl;
+    const params = currentUrl.split("?")[1];
+    urlParams.params = params
+      ? params.split("&").reduce((acc, item) => {
+          const [key, value] = item.split("=");
+          acc[key] = value;
+          return acc;
+        }, {})
+      : {};
+  }
+  return urlParams.params[name];
+};
+
+export const generateCode = async (url, match) => {
+  const codeFile = await fetch(url);
+  const rawCode = await codeFile.text();
+  if (!match) {
+    return rawCode;
+  }
+
+  const { query } = match;
+  return rawCode.replace(query, (item) => {
+    return match[item];
+  });
+};
+
+export const getAddress = (user) => {
+  return user.addr;
+};
